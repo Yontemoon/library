9922c2b91165baba082821edbb62d57cd41d78fd
Yontemoon <monteyoon1998@gmail.com>
2023-05-23 15:29:09 -0700

    Added a popup button that shows the form

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const addButton = document.querySelector('#addButton');
+const popup = document.getElementById('popup');
+
+let myLibrary = [];
+
+function Book() {
+}
+
+
+function openPopUp() {
+        popup.style.display = 'block';
+}
