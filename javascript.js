1621200d27e5b49eebf8f0c9c693855603592e90
Yontemoon <monteyoon1998@gmail.com>
2023-05-24 11:53:09 -0700

    Each form that the user inputs are now stored in a library array.

--- javascript.js
+++ javascript.js
@@ -8,25 +8,26 @@ getForm.addEventListener('submit', function(e){
     let title = document.getElementById('bookTitle').value;
     let author = document.getElementById('bookAuthor').value;
     let pages = document.getElementById('bookPages').value;
-    let yesNo = document.querySelector('input[name="yesNo"]:checked').value;
-
-    console.log(title);
+    let read = document.querySelector('input[name="yesNo"]:checked').value;
+    addBooktoLibrary(title, author, pages, read);
 })
 
 
 let myLibrary = [ 
-    {title: 'Mindhunter', author: "John Douglas", pages: 234, read: "Yes"},
+    {title: 'Mindhunter', author: "John Douglas", pages: 234, read: "Yes"}
 ];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.yesNo = read;
-    addBooktoLibrary(title, author, pages, read);
+    this.read = read;
 }
 
-
+function addBooktoLibrary(title, author, pages, read) {
+    const getObject = new Book(title, author, pages, read);
+    myLibrary.push(getObject);
+}
 
 function openPopUp() {
     popup.style.display = 'block';
@@ -36,12 +37,6 @@ function closeForm() {
     popup.style.display = 'none';
 }
 
-function addBooktoLibrary(title, author, pages, read) {
-    console.log(title);
-    console.log(author);
-    console.log(pages);
-    console.log(read);
 
-}
 
 
