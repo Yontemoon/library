c5c2201c012fdb79ef4de1b6692d22b2f0b77cbb
Yontemoon <monteyoon1998@gmail.com>
2023-05-24 11:05:02 -0700

    Successfully got the form to submit the data the user inputs.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,47 @@
 const addButton = document.querySelector('#addButton');
 const popup = document.getElementById('popup');
+const closeButton = document.getElementById('closeButton');
+let getForm = document.getElementById("getForm");
 
-let myLibrary = [];
+getForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    let title = document.getElementById('bookTitle').value;
+    let author = document.getElementById('bookAuthor').value;
+    let pages = document.getElementById('bookPages').value;
+    let yesNo = document.querySelector('input[name="yesNo"]:checked').value;
 
-function Book() {
+    console.log(title);
+})
+
+
+let myLibrary = [ 
+    {title: 'Mindhunter', author: "John Douglas", pages: 234, read: "Yes"},
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.yesNo = read;
+    addBooktoLibrary(title, author, pages, read);
 }
+
 
 
 function openPopUp() {
-        popup.style.display = 'block';
+    popup.style.display = 'block';
 }
+
+function closeForm() {
+    popup.style.display = 'none';
+}
+
+function addBooktoLibrary(title, author, pages, read) {
+    console.log(title);
+    console.log(author);
+    console.log(pages);
+    console.log(read);
+
+}
+
+
