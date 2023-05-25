3c2ce0ac3e2f99e85e2ee59c3fd948fae74e5bcb
Yontemoon <monteyoon1998@gmail.com>
2023-05-25 14:52:03 -0700

    Finally successfully added a delete button for each row of books

--- javascript.js
+++ javascript.js
@@ -2,7 +2,16 @@ const addButton = document.querySelector('#addButton');
 const popup = document.getElementById('popup');
 const closeButton = document.getElementById('closeButton');
 let getForm = document.getElementById("getForm");
+
 const library = document.getElementById("library");
+const childLibrary = library.querySelectorAll("#parent > .childLibrary")
+const titleRow = document.getElementById('titleRow');
+const authorRow = document.getElementById('authorRow');
+const pagesRow = document.getElementById('pagesRow');
+const readRow = document.getElementById('readRow');
+const buttonRow = document.getElementById('buttonRow');
+
+
 const titleLib =document.getElementById("titleLib");
 
 
@@ -27,30 +36,34 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+let numberOfBooks = myLibrary.length;
 function addBooktoLibrary(title, author, pages, read) {
     let libraryContent = "";
+    let titleContent = "";
+    let authorContent = "";
+    let pagesContent = "";
+    let readContent = "";
+    let buttonContent = "";
     const getObject = new Book(title, author, pages, read);
-    let numberOfBooks = myLibrary.push(getObject);
-    // displayLibrary(numberOfBooks);
-
+    numberOfBooks = myLibrary.push(getObject); //Returns the length of myLibrary
     for (let i = 0; i < numberOfBooks; i++) {
-        libraryContent += "<p>Title: " + myLibrary[i].title + "</p>";
-        libraryContent += "<p>Author: " + myLibrary[i].author + "</p>";
-        libraryContent += "<p>Pages: " + myLibrary[i].pages + "</p>";
-        libraryContent += "<p>Read: " + myLibrary[i].read + "</p>";
+        titleContent += `<p class=row${i}>Title: ` + myLibrary[i].title + "</p>";
+        authorContent += `<p class=row${i}>Author: ` + myLibrary[i].author + "</p>";
+        pagesContent += `<p class=row${i}>Pages: ` + myLibrary[i].pages + "</p>";
+        readContent += `<p class=row${i}>Read: ` + myLibrary[i].read + "</p>";
+        // buttonContent += document.createElement('button');
+        
+        buttonContent += `<button onclick=deleteRow(${i}) class=row${i}>Delete</button>`;
+        // buttonContent.setAttribute('id',`buttonID ${i}`);
     }
 
-    library.innerHTML = libraryContent;
+    titleRow.innerHTML = titleContent;
+    authorRow.innerHTML = authorContent;
+    pagesRow.innerHTML = pagesContent;
+    readRow.innerHTML = readContent;
+    buttonRow.innerHTML = buttonContent;
 }
 
-// function displayLibrary(numberOfBooks) {
-//     for (let i = 0; i < numberOfBooks; i++) {
-//         console.log(Book.title);
-//         // for (let j = 0; j < myLibrary.length; j++) {
-
-//         // }
-//     }
-// }
 
 function openPopUp() {
     popup.style.display = 'block';
@@ -58,6 +71,15 @@ function openPopUp() {
 
 function closeForm() {
     popup.style.display = 'none';
+}
+
+function deleteRow(rowNumber) {
+    myLibrary.splice(rowNumber, 1);
+    // numberOfBooks--;
+    let deleteRow = document.querySelectorAll(`.row${rowNumber}`);
+    deleteRow.forEach(function(everyRow) {
+        everyRow.remove();
+    })
 }
 
 
